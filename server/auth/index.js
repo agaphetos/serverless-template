06d14fc6455f896a5280706cdc2d0a9b91c60e54
Marvin Arcilla <arcilla.bumblebeen@gmail.com>
2018-04-05 15:00:37 +0800

    Add Hash Functionality

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,9 +1,12 @@
+import encrypt from './encrypt';
 import localJwt from './localJwt';
 import google from './google';
+
 
 const auth = {
   jwt: localJwt,
   google,
+  encrypt,
 };
 
 export default auth;
